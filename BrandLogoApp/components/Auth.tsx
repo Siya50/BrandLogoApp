5c14d8147bf31456c1ef956fd5626dd03d53be59
Siya50 <siya.dasagranthi@gmail.com>
2026-01-22 12:38:13 -0600

    checks password requirements

--- BrandLogoApp/components/Auth.tsx
+++ BrandLogoApp/components/Auth.tsx
@@ -1,19 +1,24 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import defaultStyles from "../app/styles/defaultStyles";
 import TextField from "../components/TextField";
 export default function Auth() {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const router = useRouter();
-
+    //Used ChatGPT to learn how to check for a number in a string
+    // 1/22/26
+    // https://chatgpt.com/share/69726e7d-5108-8013-ae16-6edff366d4c1
   const goToHome = () => {
-    if(username != "" && password != "" && password.length>=6){
+    if(username != "" && password != "" && password.length>=6 && /\d/.test(password)){
         setUsername(username);
         console.log(username, password);
         router.push("../(tabs)/Browse");
     }   
+    else{
+        Alert.alert("You need a username and a password with 6 characters and a number");
+    }
   };
 
   return (
